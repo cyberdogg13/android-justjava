c0b88aaac0cd176f09090d2900c6dbc015aec161
timvanderstam <timvanderstam@hotmail.com>
2021-09-14 01:15:09 +0200

    checkboxes toegevoegt

--- app/src/main/java/com/example/justjava2/MainActivity.java
+++ app/src/main/java/com/example/justjava2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.justjava2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,44 +15,94 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int numberofcoffee;
-    String message = "";
+    int costOfCoffee = 5;
+    int numberofcoffee = 1;
+    int chocolateCost = 2;
+    int creamCost = 1;
+    boolean whippedCream = false;
+    boolean chocolate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayquantity(numberofcoffee);
+        displayPrice(calculatePrice(numberofcoffee));
     }
+
+    public void addWhippedcream (View view){
+        CheckBox creambox = (CheckBox) view;
+        if (creambox.isChecked()){
+            whippedCream =true;
+            costOfCoffee += creamCost;
+        }
+        else {
+            whippedCream =false;
+            costOfCoffee -= creamCost;
+        }
+        displayPrice(calculatePrice(numberofcoffee));
+    }
+
+    public void addChocolate (View view){
+        CheckBox creambox = (CheckBox) view;
+        if (creambox.isChecked()){
+            chocolate =true;
+            costOfCoffee += chocolateCost;
+        }
+        else {
+            chocolate =false;
+            costOfCoffee -= chocolateCost;
+        }
+        displayPrice(calculatePrice(numberofcoffee));
+    }
+
 
     public void submitOrder(View view) {
-        message = " Thank you!";
-        displayPrice(numberofcoffee * 5);
+        displayPrice(calculatePrice(numberofcoffee));
+        createOrderSumary(calculatePrice(numberofcoffee));
 
     }
 
-    private void display(int number) {
+    private void displayquantity(int orderedCoffee) {
         TextView quantityTextview = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextview.setText(" " + number);
+        quantityTextview.setText(" " + orderedCoffee);
     }
 
-    private void displayPrice(int number) {
+    private int calculatePrice(int quantity){
+        int price = quantity * costOfCoffee;
+        return price;
+    }
+
+    private void displayPrice(int price) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number) + message);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
     }
 
     public void addCoffee(View view) {
-        numberofcoffee++;
-        display(numberofcoffee);
-        displayPrice(numberofcoffee *5);
+        if (numberofcoffee < 100){
+            numberofcoffee++;
+            displayquantity(numberofcoffee);
+            displayPrice(calculatePrice(numberofcoffee));
+        }
     }
     public void subtractCoffee(View view) {
-        numberofcoffee--;
-        display(numberofcoffee);
-        displayPrice(numberofcoffee *5);
+        if (numberofcoffee > 1){
+            numberofcoffee--;
+            displayquantity(numberofcoffee);
+            displayPrice(calculatePrice(numberofcoffee));
+        }
     }
-    private void diplaymessage(String message){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    private void createOrderSumary (int orderPrice){
+        EditText nameInput = (EditText) findViewById(R.id.naam);
+        String ordertext = "Order sumary" + "\n ";
+        ordertext += "\nname = " + nameInput.getText() +
+                "\nquantity = " + numberofcoffee +
+                "\nwhippedCream = " + whippedCream +
+                "\nChocolate = " + chocolate +
+                "\ntotal = " + NumberFormat.getCurrencyInstance().format(orderPrice) +
+                "\nThank you!";
+        TextView priceTextView = (TextView) findViewById(R.id.fullOrder);
+        priceTextView.setText(ordertext);
 
     }
 }
